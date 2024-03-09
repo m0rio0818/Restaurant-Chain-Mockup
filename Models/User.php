6f52f07e446fec257777e7c38c447404bcf76298
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-03-09 22:48:50 +0900

    initial commit

---
+++ Models/User.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Models;
+
+use DateTime;
+
+class User
+{
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private string $email;
+    private string $hashedPassword;
+    private string $phoneNumber;
+    private string $address;
+    private DateTime $birthDate;
+    private DateTime $membershipExpirationDate;
+    private string $role;
+
+    public function __construct(
+        int $id,
+        string $firstName,
+        string $lastName,
+        string $email,
+        string $password,
+        string $phoneNumber,
+        string $address,
+        DateTime $birthDate,
+        DateTime $membershipExpirationDate,
+        string $role
+    ) {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $this->phoneNumber = $phoneNumber;
+        $this->address = $address;
+        $this->birthDate = $birthDate;
+        $this->membershipExpirationDate = $membershipExpirationDate;
+        $this->role = $role;
+    }
+
+    public function login(string $password): bool
+    {
+        // Validate password with the hashed password
+        return password_verify($password, $this->hashedPassword);
+    }
+
+    public function updateProfile(string $address, string $phoneNumber): void
+    {
+        $this->address = $address;
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    public function renewMembership(DateTime $expirationDate): void
+    {
+        $this->membershipExpirationDate = $expirationDate;
+    }
+
+    public function changePassword(string $newPassword): void
+    {
+        $this->hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+    }
+
+    public function hasMembershipExpired(): bool
+    {
+        $currentDate = new DateTime();
+        return $currentDate > $this->membershipExpirationDate;
+    }
+
+
+    public function toString(): string
+    {
+        return sprintf(
+            "User ID: %d \nName : %s %s\nEmail: %s\nAddress: %s\nBirthDate: %s\nMemberShip Expiration Date: %s\nRole: %s\n",
+            $this->id,
+            $this->firstName,
+            $this->lastName,
+            $this->address,
+            $this->birthDate,
+            $this->membershipExpirationDate->format("Y-m-d"),
+            $this->role,
+        );
+    }
+
+    public function toHTML(): string
+    {
+        return sprintf(
+            "
+            <div class='user-card'>
+                <div class='avater'>SAMPLE</div>
+                <h2>%s %s</h2>
+                <p>%s</p>
+                <p>%s</p>
+                <p>%s</p>
+                <p>BirthDate : %s</p>
+                <p>MemberShip Expireation Date: %s</p>
+                <p>Role : %s</p>
+            </div>",
+            $this->firstName,
+            $this->lastName,
+            $this->email,
+            $this->phoneNumber,
+            $this->address,
+            $this->birthDate->format("Y-m-d"),
+            $this->membershipExpirationDate->format("Y-m-d"),
+            $this->role
+        );
+    }
+
+    public function  toMarkDown(): string
+    {
+        return "## User : {$this->firstName} {$this->lastName}
+                 -Email : {$this->email}
+                 -Address: {$this->address}
+                 -BirthDate : {$this->birthDate}
+                 -IsActive: {$this->hasMembershipExpired() }
+                 -Role : {$this->role}";
+    }
+
+    public function toArray(): array
+    {
+        return [
+            "id" => $this->id,
+            "firstName" => $this->firstName,
+            "lastName" => $this->lastName,
+            "email" => $this->email,
+            "password" => $this->hashedPassword,
+            "phoneNumber" => $this->phoneNumber,
+            "address" => $this->address,
+            "birthDate" => $this->birthDate,
+            "isActive" => $this->hasMembershipExpired(),
+            "role" => $this->role,
+        ];
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+// コードベースのファイルのオートロード
+use Helpers\RandomGenerator;
+spl_autoload_extensions(".php"); 
+spl_autoload_register();
+
+// composerの依存関係のオートロード
+require_once 'vendor/autoload.php';
+
+
+// クエリ文字列からパラメータを取得
+$min = $_GET['min'] ?? 5;
+$max = $_GET['max'] ?? 20;
+
+// パラメータが整数であることを確認
+$min = (int)$min;
+$max = (int)$max;
+
+// ユーザーの生成
+$users = RandomGenerator::users($min, $max);
+
+foreach($users as $user){
+    echo $user.toString() . PHP_EOL;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profiles</title>
+    <style>
+        /* ユーザーカードのスタイル */
+    </style>
+</head>
+<body>
+    <h1>User Profiles</h1>
+
+    <?php foreach ($users as $user): ?>
+    <div class="user-card">
+    </div>
+    <?php endforeach; ?>
+
+</body>
+</html>
